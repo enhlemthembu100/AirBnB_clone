d5805820991cae3c260bd0f9379c13822fba36c0
enhlemthembu100 <139383610+enhlemthembu100@users.noreply.github.com>
2024-02-26 05:33:12 +0200

    Updated

---
+++ web_static/models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Amenity class"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Attribute:
+        name: The name of the amenity.
+    """
+    name = ""
